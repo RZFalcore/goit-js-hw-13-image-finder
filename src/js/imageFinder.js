7b2b22d8f3c4ca03b503ed4dac89818c9d521575
RZFalcore <box.rz.box@gmail.com>
2020-07-01 20:20:11 +0300

    add some styling, tmps

--- src/js/imageFinder.js
+++ src/js/imageFinder.js
@@ -1,10 +1,19 @@
 import api from '../services/axios';
 import searchForm from '../templates/searchForm.hbs';
-
-api.fetchQuerry('cats');
+import listItemTmp from '../templates/listItem.hbs';
 
 const refs = {
   body: document.querySelector('body'),
+  form: document.querySelector('#search-form'),
+  galery: document.querySelector('.gallery'),
 };
 
-console.log(refs.body);
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  const querry = e.target.elements[0].value;
+  api.fetchQuerry(querry).then(data => {
+    console.log(data);
+    const markup = data.map(item => listItemTmp(item)).join('');
+    refs.galery.insertAdjacentHTML('beforeend', markup);
+  });
+});
--- src/services/axios.js
+++ src/services/axios.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 // axios.defaults.
 
 export default {
+  page: 1,
   baseURL:
     'https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12&key=13203870-f88321e1576e2ee35198d8add',
   async fetchQuerry(querry) {
     try {
-      const response = await axios.get(this.baseURL + `&q=${querry}`);
-      return console.log(response.data.hits);
+      const response = await axios.get(
+        this.baseURL + `&q=${querry}` + `&page=${this.page}`,
+      );
+
+      return response.data.hits;
     } catch (error) {
       console.error(error);
     }
